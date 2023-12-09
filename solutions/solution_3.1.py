c972f9b576874dd5c19a7793115d1e6630434f15
Will Stevens <kageus@gmail.com>
2023-12-09 17:33:00 -0500

    Day 3 solution

--- solutions/solution_3.1.py
+++ solutions/solution_3.1.py
@@ -1,73 +1,75 @@
-def isSymbolOnAdjacentSeven(xy, x, y):
-    symbols = "@#$%^&*/+-"
+def isSymbolOnAdjacentSeven(yx, x, y):
+    symbols = "@#$%^&*/+-="
     #top
     if y != 0:
         #right
-        if x != len(xy[0])-1:
-            if xy[y-1][x+1] in symbols:
+        if x != len(yx[0])-1:
+            if yx[y-1][x+1] in symbols:
                 return 1
         #mid
-        if xy[y-1][x] in symbols:
+        if yx[y-1][x] in symbols:
                 return 1
         #left
         if x != 0:
-            if xy[y-1][x-1] in symbols:
+            if yx[y-1][x-1] in symbols:
                 return 1
     #mid left
     if x != 0:
-        if xy[y][x-1] in symbols:
+        if yx[y][x-1] in symbols:
             return 1
     #bottom
-    if y != len(xy)-1:
+    if y != len(yx)-1:
         #right
-        if x != len(xy[0])-1:
-            if xy[y+1][x+1] in symbols:
+        if x != len(yx[0])-1:
+            if yx[y+1][x+1] in symbols:
                 return 1
         #mid
-        if xy[y+1][x] in symbols:
+        if yx[y+1][x] in symbols:
             return 1
         #left
         if x != 0:
-            if xy[y+1][x-1] in symbols:
+            if yx[y+1][x-1] in symbols:
                 return 1
     return 0
     
 
 dataInput = open("../inputs/input_3.txt").read().split("\n")
 # dataInput = open("../inputs/sample_input_3.txt").read().split("\n")
-xy = []
+yx = []
 
 for line in dataInput:
     x = []
     for char in line:
         x.append(char)
-    xy.append(x)
-print(xy)
+    yx.append(x)
+print(yx)
 isSymbolAdjacent = 0
 holdDigits = ""
 totalSum = 0
-for y in range(0, len(xy)):
-    for x in range(0, len(xy[0])):
-        if not xy[y][x].isdigit():
-            print(x, y, xy[y][x], 'NOT', isSymbolAdjacent, holdDigits, totalSum)
-            if xy[y][x] in "@#$%^&*/+-":
+for y in range(0, len(yx)):
+    for x in range(0, len(yx[0])):
+        if not yx[y][x].isdigit():
+            if yx[y][x] not in ".":
                 isSymbolAdjacent = 1
             if holdDigits and isSymbolAdjacent:
                 totalSum += int(holdDigits)    
+            # print(x, y, yx[y][x], 'NOT', isSymbolAdjacent, holdDigits, totalSum)
+            # input()
             holdDigits = ""
             isSymbolAdjacent = 0
 
             continue
 
         if not isSymbolAdjacent:
-            isSymbolAdjacent = isSymbolOnAdjacentSeven(xy, x, y)
-        holdDigits += xy[y][x]
-        print(x, y, xy[y][x], '   ', isSymbolAdjacent, holdDigits, totalSum)
-    
+            isSymbolAdjacent = isSymbolOnAdjacentSeven(yx, x, y)
+        holdDigits += yx[y][x]
+        # print(x, y, yx[y][x], '   ', isSymbolAdjacent, holdDigits, totalSum)
+        # input()
     # process any adjacent helds before new row
     if holdDigits and isSymbolAdjacent:
         totalSum += int(holdDigits)    
         holdDigits = ""
         isSymbolAdjacent = 0
-    print(x, y, xy[y][x], '   ', isSymbolAdjacent, holdDigits, totalSum, 'end of row')
+    # print(x, y, yx[y][x], '   ', isSymbolAdjacent, holdDigits, totalSum, 'end of row')
+    # input()
 print(totalSum)
---
+++ solutions/solution_3.2.py
@@ -0,0 +1,163 @@
+def getGearRatio(yx, x, y):
+    # print("getting ratio", yx[y][x])
+    # input()
+    partBuilder = ""
+    foundParts = []
+    #top
+    if y != 0:
+        #top left
+        if x != 0:
+            if yx[y-1][x-1].isdigit():
+                partBuilder += yx[y-1][x-1]
+                partBuilder = eatLeft(yx, y-1, x-1, partBuilder)
+        #top center
+        if partBuilder:
+            if not yx[y-1][x].isdigit():
+                foundParts.append(partBuilder)
+                partBuilder = ""
+            else:
+                partBuilder += yx[y-1][x]
+        else:
+            if yx[y-1][x].isdigit():
+                partBuilder += yx[y-1][x]
+
+
+        #top right
+        if x != len(yx[0])-1:
+            if partBuilder:
+                if not yx[y-1][x+1].isdigit():
+                    foundParts.append(partBuilder)
+                    partBuilder = ""
+                else:
+                    partBuilder += yx[y-1][x+1]
+                    partBuilder = eatRight(yx, y-1, x+1, partBuilder)
+                    foundParts.append(partBuilder)
+                    partBuilder = ""
+            else:
+                if not yx[y-1][x+1].isdigit():
+                    partBuilder = ""
+                else:
+                    partBuilder += yx[y-1][x+1]
+                    partBuilder = eatRight(yx, y-1, x+1, partBuilder)
+                    foundParts.append(partBuilder)
+                    partBuilder = ""
+        if partBuilder:
+            foundParts.append(partBuilder)
+            partBuilder = ""
+        partBuilder = ""
+    partBuilder = ""
+
+    #mid left
+    if x != 0:
+        if yx[y][x-1].isdigit():
+            partBuilder += yx[y][x-1]
+            partBuilder = eatLeft(yx, y, x-1, partBuilder)
+            foundParts.append(partBuilder)
+            partBuilder = ""
+
+    #mid right
+    if x != len(yx[0])-1:
+        if yx[y][x+1].isdigit():
+            partBuilder += yx[y][x+1]
+            partBuilder = eatRight(yx, y, x+1, partBuilder)
+            foundParts.append(partBuilder)
+            partBuilder = ""
+    
+
+    #bottom
+    if y != len(yx)-1:
+        #bottom left
+        if x != 0:
+            if yx[y+1][x-1].isdigit():
+                partBuilder += yx[y+1][x-1]
+                partBuilder = eatLeft(yx, y+1, x-1, partBuilder)
+            else:
+                partBuilder = ""
+        #bottom center
+        if partBuilder:
+            if not yx[y+1][x].isdigit():
+                foundParts.append(partBuilder)
+                partBuilder = ""
+            else:
+                partBuilder += yx[y+1][x]
+        else:
+            if yx[y+1][x].isdigit():
+                partBuilder += yx[y+1][x]
+
+        #bottom right
+        if x != len(yx[0])-1:
+            if partBuilder:
+                if not yx[y+1][x+1].isdigit():
+                    foundParts.append(partBuilder)
+                    partBuilder = ""
+                else:
+                    partBuilder += yx[y+1][x+1]
+                    partBuilder = eatRight(yx, y+1, x+1, partBuilder)
+
+                    foundParts.append(partBuilder)
+                    partBuilder = ""
+            else:
+                if not yx[y+1][x+1].isdigit():
+                    partBuilder = ""
+                else:
+                    partBuilder += yx[y+1][x+1]
+                    partBuilder = eatRight(yx, y+1, x+1, partBuilder)
+                    foundParts.append(partBuilder)
+                    partBuilder = ""
+        if partBuilder:
+            foundParts.append(partBuilder)
+            partBuilder = ""
+    partBuilder = ""
+
+    # print("foundParts",foundParts)
+    if len(foundParts) == 2:
+        # print('got a gear with values:', foundParts[0], foundParts[1])
+        # input()
+        return int(foundParts[0]) * int(foundParts[1])
+    
+    # print('not a gear')
+    # input()
+    return 0
+    
+def eatLeft(yx, y, x, partBuilder):
+    # print('eat left', partBuilder)
+    if x != 0:
+        if yx[y][x-1].isdigit():
+            partBuilder = yx[y][x-1] + partBuilder
+            partBuilder = eatLeft(yx, y, x-1, partBuilder)
+        else:
+            return partBuilder
+    return partBuilder
+
+
+def eatRight(yx, y, x, partBuilder):
+    # print('eat right', partBuilder)
+    if x != len(yx[0])-1:
+        if yx[y][x+1].isdigit():
+            partBuilder = partBuilder + yx[y][x+1]
+            partBuilder = eatRight(yx, y, x+1, partBuilder)
+        else:
+            return partBuilder
+    return partBuilder
+
+dataInput = open("../inputs/input_3.txt").read().split("\n")
+# dataInput = open("../inputs/sample_input_3.txt").read().split("\n")
+yx = []
+
+for line in dataInput:
+    x = []
+    for char in line:
+        x.append(char)
+    yx.append(x)
+
+totalSum = 0
+for y in range(0, len(yx)):
+    for x in range(0, len(yx[0])):
+        if yx[y][x] not in "*":
+            # print(x, y, yx[y][x], 'NOT', totalSum)
+            continue
+        totalSum += getGearRatio(yx, x, y) 
+        # print(x, y, yx[y][x], 'GEAR CHECK', totalSum)
+    print('end of row:', y+1 )
+
+print(totalSum)
