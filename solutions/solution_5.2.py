27f95138eb254b7ad157ca50ed40554eb79554e1
Will Stevens <kageus@gmail.com>
2023-12-15 23:08:07 -0500

    reworking day 5 in reverse

--- solutions/solution_5.2.py
+++ solutions/solution_5.2.py
@@ -1,16 +1,17 @@
-mapKey = ['seed-to-soil', 'soil-to-fertilizer', 'fertilizer-to-water', 'water-to-light', 'light-to-temperature', 'temperature-to-humidity', 'humidity-to-location']
-# dataInput = open("../inputs/input_5").read().split("\n\n")
-dataInput = open("../inputs/sample_input_5").read().split("\n\n")
+dataInput = open("../inputs/input_5").read().split("\n\n")
+# dataInput = open("../inputs/sample_input_5").read().split("\n\n")
+
+# extract the seed range data
 seedDataRaw = list(map(lambda i: int(i), dataInput.pop(0)[7:].split(" ")))
 seedRanges = []
 for i in range(0, len(seedDataRaw),2):
     seedRanges.append([seedDataRaw[i], seedDataRaw[i+1]])
-# print(seedRanges)
-iterations = 0
-for seedRange in seedRanges:
-    iterations += seedRange[1]
-print(f"{iterations:,}")
+seedRanges = sorted(seedRanges, key=lambda x: x[0])
+
+print(seedRanges)
 input()
+
+# extract the almanac from raw data
 almanac = []
 for maps in dataInput:
     submaps = maps.split('\n', 1)[1].split("\n")
@@ -18,33 +19,19 @@ for maps in dataInput:
     for mapRange in submaps:
         mapTypes.append(list(map(lambda i: int(i), mapRange.split(" "))))
     almanac.append(mapTypes)
-# print(almanac)
-# input()
-lowestConversion = float('inf')
+for i in range(0, len(almanac)):
+    almanac[i] = sorted(almanac[i], key=lambda x: x[0])
 
-iterCount = iterations
-for seedRange in seedRanges:
-    print(seedRange)
-    for seed in range(seedRange[0], seedRange[1]+seedRange[0]):
-        iterCount -= 1
-        print(f"{iterCount:,} of {iterations:,} left")
-        conversion = seed
-        i = 0
-        for mapTypes in almanac:
-            # print("Map Type:", mapKey[i], mapTypes)
-            for submap in mapTypes:
-                # print(submap)
-                if conversion in range(submap[1], submap[1]+submap[2]):
-                    # print(conversion, "in range:", submap[1], "to", submap[1]+submap[2])
-                    conversion = submap[0]+ (conversion - submap[1])
-                    # print("after conversion", conversion)
-                    break
-            # print("Conversion: ", conversion)
-            i +=1
-        # print("Original:", seed, "Full Conversion: ", conversion)
-        # input()
-        if conversion < lowestConversion:
-            lowestConversion = conversion
-    if iterCount % 10 == 0:
-        print("Conversion %:", iterCount)
-print("Lowest location:", lowestConversion)
+for potential in range(0, 10000000000):
+    print(potential)
+    original = potential
+    for mapType in reversed(almanac):
+        for subrange in mapType:
+            if potential in range(subrange[0], subrange[0]+subrange[2]):
+                potential = subrange[1] + (potential - subrange[0])
+                break
+    for seedRange in seedRanges:
+        if potential in range(seedRange[0], seedRange[0]+seedRange[1]):
+            print(f"lowest found: {original}")
+            input()
+
